21e0c781d95c78f5d29e19f270865ddc4db39edf
MSlezak2 <michalsleezak@gmail.com>
2022-07-11 17:14:24 +0200

    Initial commit
    Pseudo-code draft of the 'main' program loop has been prepared.

---
+++ GameHandler.cpp
@@ -0,0 +1,44 @@
+#include "GameHandler.h"
+
+void GameHandler::settings() {
+	// TODO: Main menu
+
+	// Zainicjowac obiekt player
+
+	// Wybor poziomu trudnosci
+
+	// Zainicjowanie obiektu Board - tablica minesBoard:
+		// rozlosowanie min
+		// ustalenie ilo?ci min w sasiedztwie
+
+	// Zainicjowanie obiektu Board - tablica currentBoard:
+		// wpisanie zer do ka?dej komórki
+
+}
+
+void GameHandler::gameLoop() {
+
+	while (/*sprawdzanie warunku zako?czenia gry*/ true) {
+
+		// wyswietlanie tablicy
+
+		// wybor czynnosci (flaga czy normalne klikniecie)
+
+		// pobranie wspolrzednych od uzytkownika
+
+		// sprawdzenie czy weszlismy na mine
+			// tak:
+			// wyswietlenie "X" (symbolu miny) w tym miejscu (wpisanie -1 do currentBoard)
+			// przegrana
+
+			// nie:
+			// sprawdzenie ilosci min w sasiedztwie:
+				// zero:
+				// wyswietlenie puste pole (wpisanie -3 do currentBoard)
+				// odwiedzenie wszystkich sasiednich komorek (rekurencja)
+
+				// rozna od zera:
+				// wyswietlenie ilosci min w sasiedztwie (wpisanie ... do currentBoard)
+	}
+}
+
---
+++ GameHandler.h
@@ -0,0 +1,9 @@
+#pragma once
+class GameHandler {
+public:
+	void settings();
+	void gameLoop();
+private:
+	//Player player;
+
+};
