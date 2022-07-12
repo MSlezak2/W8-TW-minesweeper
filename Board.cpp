692503d1763a6e1ca1b277f03cbb908360e7c700
salmaro <marcin.salamon@motorolasolutions.com>
2022-07-12 15:31:35 +0200

    Add settleMines, checkNeighbouringArea

--- Board.cpp
+++ Board.cpp
@@ -17,7 +17,67 @@ public:
 		minesBoard.assign(tempBoard.begin(), tempBoard.end());
 	}
 	bool isThereAMine(int x, int y) {
-		return (minesBoard[x][y] == -1) ? true : false;
+		return (minesBoard[x][y] < 0) ? true : false;
+	}
+	void Board::settleMines(int difficultyLevel){
+		int mines{};
+		if (difficultyLevel == 1) {
+			mines = 10;
+		}
+		else if (difficultyLevel == 2) {
+			mines = 40;
+		}
+		else if (difficultyLevel == 3) {
+			mines = 99;
+		}
+
+		srand(static_cast<int>(time(nullptr)));
+		for (int i = 1; i <= mines; i++) {
+			int x = rand() % size;
+			int y = rand() % size;
+			if (minesBoard.at(x).at(y) == 0)
+				minesBoard.at(x).at(y) = -10;
+			else
+				i--;
+		}
+
+		checkNeighbouringArea();
+	}
+	void Board::checkNeighbouringArea(){
+		std::vector<int> emptyVector(minesBoard.size());
+
+		minesBoard.insert(minesBoard.begin(), emptyVector);
+		minesBoard.push_back(emptyVector);
+
+		for (auto& e : minesBoard) {
+			e.insert(e.begin(), 0);
+			e.push_back(0);
+		}
+
+		for (size_t i{ 1 }; i < minesBoard.size() - 1; i++) {
+			for (size_t j{ 1 }; j < minesBoard[i].size() - 1; j++) {
+				if (minesBoard[i][j] < 0) {
+
+					minesBoard[i - 1][j - 1]++;
+					minesBoard[i - 1][j]++;
+					minesBoard[i - 1][j + 1]++;
+					minesBoard[i][j - 1]++;
+					minesBoard[i][j + 1]++;
+					minesBoard[i + 1][j - 1]++;
+					minesBoard[i + 1][j]++;
+					minesBoard[i + 1][j + 1]++;
+
+				}
+
+			}
+		}
+		minesBoard.erase(minesBoard.begin());
+		minesBoard.pop_back();
+		for (auto& e : minesBoard) {
+			e.erase(e.begin());
+			e.pop_back();
+		}
+
 	}
 };
 
--- Board.h
+++ Board.h
@@ -6,7 +6,10 @@ public:
 	//int detectedBombs;
 	std::vector<std::vector<int>> currentBoard;
 	std::vector<std::vector<int>> minesBoard;
+
 	Board(int size);
 	bool isThereAMine(int x, int y);
+	void settleMines(int difficultyLevel); //difficultyLevel (9, 16, 24)
+	void checkNeighbouringArea();
 };
 
